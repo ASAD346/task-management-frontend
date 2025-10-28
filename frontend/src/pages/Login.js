edb2dacda2919e9dc39cf9b8b5cdffcb22b59714
ASAD346 <156499852+ASAD346@users.noreply.github.com>
2025-10-28 17:07:35 +0500

    updated login js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -64,14 +64,14 @@ const Login = () => {
           </button>
         </form>
 
-        <div className="login-help">
+        {/* <div className="login-help">
           <p>Default credentials for testing:</p>
           <ul>
             <li>Admin: admin@example.com / password123</li>
-            {/*<li>Manager: manager@example.com / password123</li>*/}
-            {/*<li>User: user@example.com / password123</li>*/}
+            <li>Manager: manager@example.com / password123</li>
+            <li>User: user@example.com / password123</li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </div>
   );
